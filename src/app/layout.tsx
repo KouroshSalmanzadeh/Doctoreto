327d2964a5ca127720f8426cf6392ea7674d4584
Kourosh <136854787+KouroshSalmanzadeh@users.noreply.github.com>
2024-12-14 15:59:22 +0330

    Lesson 1 (#2)
    * feat: [INIT] Next.js framework for the project
    
    * feat: [ADD] new font
    chore: [REMOVE] unnecessary code and clean up default CSS

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from 'next/font/google'
+import "./globals.css";
+
+const vazirmatn = Vazirmatn({
+  subsets: ['latin', 'arabic'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "دُکترِتو",
+  description: "پلتفرم جامع جستجوی دکتر و رزور نوبت آنلاین",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>به دکترتو خوش آمدید!</h1>
+}

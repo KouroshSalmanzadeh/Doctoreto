961642b0e287aa96af971dd46656f87d7d478d36
Kourosh Salmanzadeh <kouroshsalmanzadeh@gmail.com>
2024-12-19 00:22:39 +0330

    chore: [ADD] prettier and format all files of project
    chore: [ADD] prettier commands to package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Vazirmatn } from 'next/font/google'
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 
 const vazirmatn = Vazirmatn({
-  subsets: ['latin', 'arabic'],
-  display: 'swap',
-})
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "دُکترِتو",

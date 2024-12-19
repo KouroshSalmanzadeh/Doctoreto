04bbe8560e13508bae2585f8b16637d2a06580b3
Kourosh Salmanzadeh <kouroshsalmanzadeh@gmail.com>
2024-12-19 15:01:48 +0330

    feat: [ADD] footer component
    feat: [ADD] icons components to use footer
    
    feat: [ADD] multiple images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+
 import "./globals.css";
+
 import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
 
 const vazirmatn = Vazirmatn({
   subsets: ["latin", "arabic"],
@@ -23,6 +26,10 @@ export default function RootLayout({
       <body>
         <HeaderComponent />
         <main>{children}</main>
+        <p className="tagline">
+          نوبت دهی پزشکی، سامانه نوبت دهی اینترنتی بیمارستان و پزشکان
+        </p>
+        <FooterComponent />
       </body>
     </html>
   );

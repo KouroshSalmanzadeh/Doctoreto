1a63250fc966e573a2a45e8395b9c9288dbc0e17
Kourosh Salmanzadeh <kouroshsalmanzadeh@gmail.com>
2024-12-19 15:21:59 +0330

    feat: [ADD] search component and used on the page.tsx (home page)
    feat: [ADD] logo as component
    
    feat: [ADD] search and location icon
    
    chore: [CLEAN] codes with prettier

--- src/logo/doctoreto.logo.tsx
+++ src/logo/doctoreto.logo.tsx
@@ -1,12 +1,12 @@
 import React, { SVGProps } from "react";
 
-export default function doctoretoLogo(props: SVGProps<SVGSVGElement>) {
+export default function DoctoretoLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 32 32"
-      width="256px"
-      height="256px"
+      width="1em"
+      height="1em"
       {...props}
     >
       <path

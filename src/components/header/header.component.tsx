6c02c0f6a7fcfe4108bd03a95fefb090aba5a6dd
Kourosh Salmanzadeh <kouroshsalmanzadeh@gmail.com>
2024-12-27 15:14:25 +0330

    feat: [ADD] toggle theme
    style: [CHANGE] style of header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 
 import styles from "./header.module.css";
 import data from "./itemNav.json";
@@ -8,9 +8,77 @@ import data from "./itemNav.json";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+import MingcuteSunFill from "@/icons/MingcuteSunFill";
+import MingcuteMoonFill from "@/icons/MingcuteMoonFill";
+
+import { flushSync } from "react-dom";
+
 export default function HeaderComponent(): ReactElement {
   const pathname = usePathname();
 
+  const ref = useRef<HTMLLabelElement>(null);
+
+  const [theme, setTheme] = useState<boolean>(false);
+
+  useEffect(() => {
+    const activeElement = document.querySelector(`a.${styles.active}`) as HTMLElement | null;
+
+    if (activeElement) {
+      const width = activeElement.offsetWidth;
+      document.documentElement.style.setProperty("--line-li", `${width}px`);
+  
+      const leftOffset = activeElement.offsetLeft;
+      document.documentElement.style.setProperty("--offset-left", `${leftOffset}px`);
+    }
+
+  }, [pathname]);
+
+  const changeTheme = async () => {
+
+    await document.startViewTransition(()=>{
+      flushSync(()=>{
+        if (theme) {
+
+          setTheme(!theme);
+          document.documentElement.setAttribute("data-theme", "dark");
+
+        } else {
+
+          setTheme(!theme);
+          document.documentElement.setAttribute("data-theme", "light")
+        }
+      });
+    }).ready;
+    if (ref.current) {
+      const { top, right } = ref.current.getBoundingClientRect();
+      const x = right;
+      const y = top;
+      const left = window.innerWidth - right;
+      const bottom = window.innerHeight - top;
+
+      // Calculates the radius of circle that can cover the screen
+      const maxRadius = Math.hypot(
+        Math.max(left, right),
+        Math.max(top, bottom),
+      );
+
+      document.documentElement.animate(
+        {
+          clipPath: [
+            `circle(0px at ${x}px ${y}px)`,
+            `circle(${maxRadius}px at ${x}px ${y}px)`,
+          ],
+        },
+        {
+          duration: 500,
+          easing: 'ease-in-out',
+          pseudoElement: '::view-transition-new(root)',
+        }
+      );
+    }
+  };
+
+  
   return (
     <header className={styles.header}>
       <nav>
@@ -27,7 +95,14 @@ export default function HeaderComponent(): ReactElement {
           ))}
         </ul>
       </nav>
-      <button className={styles.cta}>ورود | ثبت نام</button>
+      <div className={styles.section_left_nav}>
+        <label ref={ref} htmlFor={styles.input_toggle_nav} className={styles.toggle_theme}>
+          <input type="checkbox" id={styles.input_toggle_nav} onClick={changeTheme} />
+          <MingcuteSunFill className={styles.theme_icon_light} />
+          <MingcuteMoonFill className={styles.theme_icon_dark} />
+        </label>
+        <button className={styles.cta}>ورود | ثبت نام</button>
+      </div>
     </header>
   );
 }

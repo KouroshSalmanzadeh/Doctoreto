f358658649532a260c869915194f532149842652
Kourosh Salmanzadeh <kouroshsalmanzadeh@gmail.com>
2024-12-27 20:25:24 +0330

    feat: [ADD] error page
    feat: [ADD] new style for scrollbar

---
+++ src/app/error.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { ReactElement } from 'react'
+
+import styles from "./error.module.css"
+
+import errorImg from "@/assets/illastrations/error-img.svg"
+import Image from 'next/image'
+
+type Props= {
+    error: Error & {digest?: string};
+    reset: () => void;
+} 
+
+export default function Error ({error, reset}: Props): ReactElement {
+  return (
+    <div className={styles.error}>
+        <div className={styles.text}>
+            <span className={styles.status_code}>اوه اوه!</span>
+            <h2>یک خطای غیر منتظره رخ داد!</h2>
+            <p>با عرض پوزش، لطفن با تیم پشتیبانی تماس بگیرید</p>
+        </div>
+        <div className={styles.vector}>
+            <Image src={errorImg} alt='error' />
+        </div>
+        <div className={styles.actions}>
+            <button onClick={reset}>تلاش مجدد</button>
+        </div>
+        <div className={styles.trace}>
+            <details>
+                <summary>خطاهای رخ داده:</summary>
+                <pre className='scroll' dir="ltr">{error.stack}</pre>
+            </details>
+        </div>
+    </div>
+  )
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,15 +10,15 @@ import GlobalSearchBoxComponent from '@/components/global-search-box/global-sear
 export default function NotFound (): ReactElement {
   return (
     <div className={styles.not_found}>
-        <div className={styles.vector}>
-            <Image src={notFoundImg} alt='' />
-        </div>
         <div className={styles.text}>
             <h2>صفحه مورد نظر یافت نشد!</h2>
             <p>لطفا از طریق کادر جستجو، دکتر یا مرکز درمانی مد نظر خود را جستجو کنید.</p>
         </div>
         <div className={styles.search_box}>
             <GlobalSearchBoxComponent />
+        </div>
+        <div className={styles.vector}>
+            <Image src={notFoundImg} alt='' />
         </div>
     </div>
   )
